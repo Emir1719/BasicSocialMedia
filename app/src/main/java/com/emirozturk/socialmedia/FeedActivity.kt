a3ce15dc26bbeaaace49d870799dd40ef7009fa7
Emir Öztürk <emirzr56@gmail.com>
2023-09-28 19:55:54 +0300

    login, register, sign out added

--- app/src/main/java/com/emirozturk/socialmedia/FeedActivity.kt
+++ app/src/main/java/com/emirozturk/socialmedia/FeedActivity.kt
@@ -1,14 +1,49 @@
 package com.emirozturk.socialmedia
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.Menu
+import android.view.MenuItem
 import com.emirozturk.socialmedia.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.signOut -> signOut()
+            R.id.addPost -> addPost()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun addPost() {
+        val intent = Intent(this, UploadActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/emirozturk/socialmedia/MainActivity.kt
+++ app/src/main/java/com/emirozturk/socialmedia/MainActivity.kt
@@ -1,24 +1,66 @@
 package com.emirozturk.socialmedia
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.emirozturk.socialmedia.ToastMessage.showMessage
 import com.emirozturk.socialmedia.databinding.ActivityMainBinding
-import com.emirozturk.socialmedia.databinding.ActivityUploadBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private var email: String = ""
+    private var password: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //Eğer kullanıcı daha önceden giriş yaptıysa diğer ekrana geç.
+            gotoFeedActivity()
+        }
     }
 
     fun register(view: View) {
+        email = binding.editMail.text.toString()
+        password = binding.editPassword.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            gotoFeedActivity()
+        }.addOnFailureListener {
+            showMessage(this, it.localizedMessage!!)
+        }
     }
 
     fun login(view: View) {
+        email = binding.editMail.text.toString()
+        password = binding.editPassword.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            gotoFeedActivity()
+        }.addOnFailureListener {
+            showMessage(this, it.localizedMessage!!)
+        }
+    }
+
+    private fun gotoFeedActivity() {
+        val intent = Intent(this, FeedActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
