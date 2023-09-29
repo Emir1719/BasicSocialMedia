e312c2c22f66339a6f40f586df738b3a09441532
Emir Öztürk <emirzr56@gmail.com>
2023-09-29 18:07:17 +0300

    gallery permission added

--- app/src/main/java/com/emirozturk/socialmedia/UploadActivity.kt
+++ app/src/main/java/com/emirozturk/socialmedia/UploadActivity.kt
@@ -1,16 +1,32 @@
 package com.emirozturk.socialmedia
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.emirozturk.socialmedia.ToastMessage.showMessage
 import com.emirozturk.socialmedia.databinding.ActivityUploadBinding
+import com.google.android.material.snackbar.Snackbar
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        registerLauncher()
     }
 
     fun upload(view: View) {
@@ -18,6 +34,50 @@ class UploadActivity : AppCompatActivity() {
     }
 
     fun selectImage(view: View) {
+        var permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        if (Build.VERSION.SDK_INT >= 33) {
+            permission =  Manifest.permission.READ_MEDIA_IMAGES;
+        }
 
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            //İznimiz yok. İzin isteyeceğiz.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                Snackbar.make(view, "Permission needed to view your gallery.", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    permissionLauncher.launch(permission)
+                }.show()
+            }
+            else {
+                permissionLauncher.launch(permission)
+            }
+        }
+        else {
+            //Eğer izin varsa.
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intent)
+        }
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val intent = it.data
+                intent?.let {
+                    selectedPicture = it.data
+                    selectedPicture?.let {
+                        binding.imageUserPost.setImageURI(it)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intent)
+            }
+            else {
+                showMessage(this, "Permission needed!")
+            }
+        }
     }
 }
